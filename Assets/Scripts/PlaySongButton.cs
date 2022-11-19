78136d08cbdcc29d60f2c46a0db80afa6c7ec000
Lorran de Barros <lorranbarrinha@gmail.com>
2022-11-19 17:06:51 -0300

    Max characters in song button

--- Assets/Scripts/PlaySongButton.cs
+++ Assets/Scripts/PlaySongButton.cs
@@ -5,6 +5,15 @@ using TMPro; //TextMeshPro, a text package :)
 
 public class PlaySongButton : MonoBehaviour
 {
+
+    public TextMeshProUGUI songNameText; //TMP text component
+    public int _characLimit; //Max characters to display
+
+    private void Awake()
+     {
+        songNameText = GetComponentInChildren<TextMeshProUGUI>(); //Get the TMP text component
+    }
+
     public void PlaySong()
     {
         GameController _gc = GameObject.Find("Canvas").GetComponent<GameController>(); //Finds the game controller
@@ -13,5 +22,13 @@ public class PlaySongButton : MonoBehaviour
         
         _gc.PlaySong(_songName); //Plays the song
         
+    }
+
+    private void Update()
+    {
+        if (songNameText.text.Length > _characLimit) //If the song name is longer than the limit
+        {
+            songNameText.text = songNameText.text.Substring(0, _characLimit) + "..."; //Shorten the song name
+        }    
     }
 }
